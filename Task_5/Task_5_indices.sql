83865195a5923ea488455f52388bdf2688458d32
Shevchenko Artem <shevchenkoaa@sgu.ru>
2016-05-17 16:54:29 +0300

    Uncommented usable querys

--- Task_5/Task_5_indices.sql
+++ Task_5/Task_5_indices.sql
@@ -1,19 +1,18 @@
 -- Covering Index
 -- Nonclustered type
--- Query:	SELECT place FROM doc_ticket WHERE place > 200;
+-- Query:  SELECT place FROM doc_ticket WHERE place > 200;
 
--- CREATE NONCLUSTERED INDEX cl_place ON doc_ticket([place]);
+CREATE NONCLUSTERED INDEX cl_place ON doc_ticket([place]);
 
 -- Composite index
 -- Clustered type
--- Query:	
-SELECT a.name, c.country FROM cat_airports AS a
-				JOIN add_country AS c ON a.country = c.id;
+-- Query:   SELECT a.name, c.country FROM cat_airports AS a
+--				JOIN add_country AS c ON a.country = c.id;
 
--- CREATE CLUSTERED INDEX cl_country ON add_country([id], [country]);
+CREATE CLUSTERED INDEX cl_country ON add_country([id], [country]);
 
 -- Unique index
 -- Clustered index
--- Query:	SELECT name FROM cat_airports WHERE name LIKE 'Ýé%';
+-- Query:  SELECT name FROM cat_airports WHERE name LIKE 'Ï%';
 
--- CREATE CLUSTERED INDEX air_name ON cat_airports([name]);
+CREATE CLUSTERED INDEX air_name ON cat_airports([name]);

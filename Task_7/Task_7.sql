bea71ceef2adff299822d4d2550378a53331fe7e
Shevchenko Artem <shevchenkoaa@sgu.ru>
2016-05-18 18:38:15 +0300

    Removed commentaries

--- Task_7/Task_7.sql
+++ Task_7/Task_7.sql
@@ -1,38 +1,40 @@
  -- Procedure for counting and summing
  -- classes sold ticket revenue
---CREATE PROCEDURE soldTicketQtySum 
---	@FlightNum AS INT,
---	@ClassType AS INT
---AS
---	SELECT dt.flight_report,
---				ac.class_name, 
---				COUNT(*) AS quantity,
---				SUM(dt.price) AS Total_Sum 
---	FROM doc_ticket AS dt
---	JOIN add_class AS ac 
---	ON ac.id = dt.class
---	WHERE dt.class = @ClassType AND sold = 1 AND dt.flight_report = @FlightNum
---	GROUP BY dt.flight_report, ac.class_name
---GO
+CREATE PROCEDURE soldTicketQtySum
+	@FlightNum AS INT,
+	@ClassType AS INT
+  AS
+	SELECT dt.flight_report,
+				ac.class_name,
+				COUNT(*) AS quantity,
+				SUM(dt.price) AS Total_Sum
+	FROM doc_ticket AS dt
+	JOIN add_class AS ac
+	ON ac.id = dt.class
+	WHERE dt.class = @ClassType AND sold = 1 AND dt.flight_report = @FlightNum
+	GROUP BY dt.flight_report, ac.class_name
+GO
 
- --EXEC soldTicketQtySum  0, 1;
- --GO
+EXEC soldTicketQtySum  0, 1;
+GO
 
-
---CREATE PROCEDURE checkFlgAva 
---	@dateCheck DATETIME,
---	@result VARCHAR(50) OUTPUT
---AS
---	IF EXISTS(SELECT * FROM cat_flight WHERE start_time LIKE @dateCheck)
---		BEGIN
---			SELECT * FROM cat_flight WHERE start_time LIKE @dateCheck;
---			SET @result = 'Available';
---		END
---	ELSE
---		BEGIN
---			SET @result = 'Not Available';
---		END
---GO
+-- Procedure for checking
+-- availability of flight
+-- by DATETIME
+CREATE PROCEDURE checkFlgAva
+	@dateCheck DATETIME,
+	@result VARCHAR(50) OUTPUT
+  AS
+	IF EXISTS(SELECT * FROM cat_flight WHERE start_time LIKE @dateCheck)
+		BEGIN
+			SELECT * FROM cat_flight WHERE start_time LIKE @dateCheck;
+			SET @result = 'Available';
+		END
+	ELSE
+		BEGIN
+			SET @result = 'Not Available';
+		END
+GO
 
 DECLARE @res VARCHAR(50);
 
